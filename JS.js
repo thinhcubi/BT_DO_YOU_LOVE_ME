2ced73121667713b518cf10337def8b47b885afb
thine <thinhcubi27@gmail.com>
2021-04-09 16:47:37 +0700

    JS.js

---
+++ JS.js
@@ -0,0 +1,10 @@
+function setButton(){
+alert("I FUCK YOU")
+}
+function mouseOver(){
+    let x=Math.floor(Math.random()*window.innerHeight)
+    let y=Math.floor(Math.random()*window.innerWidth)
+    document.getElementById("No").style.left= x + "px";
+    document.getElementById("No").style.top= y + "px";
+
+}
